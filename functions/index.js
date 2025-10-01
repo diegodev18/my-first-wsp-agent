bd1b7b9641ed20da84642baffa2e91d8d124aa9e
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-09-30 23:40:27 -0600

    Add API endpoint to handle requests and log information

--- functions/index.js
+++ functions/index.js
@@ -2,4 +2,10 @@ import { setGlobalOptions } from "firebase-functions";
 import { onRequest } from "firebase-functions/v1/https";
 import logger from "firebase-functions/logger";
 
+
 setGlobalOptions({ maxInstances: 10 });
+
+export const api = onRequest((req, res) => {
+    logger.info("Hello logs!", { structuredData: true });
+    res.send("Hello from Firebase!");
+});
