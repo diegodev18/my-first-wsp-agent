b3a85b8e002e53152b27b440cb86cc1a45604187
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-01 17:32:44 -0600

    Implement add function to store and update user memory in Firestore

--- functions/src/utils/db/memory.js
+++ functions/src/utils/db/memory.js
@@ -17,3 +17,23 @@ export const get = async (waId) => {
         return "";
     }
 };
+
+export const add = async (waId, newMemory) => {
+    const collection = db.collection("memory");
+
+    const docRef = collection.doc(waId);
+    if (!docRef) {
+        collection.add({ waId, memory: newMemory });
+        return;
+    }
+
+    const doc = await docRef.get();
+    if (doc.exists) {
+        const updatedMemory = doc.data().memory && doc.data().memory.trim() !== "" ?
+            `${doc.data().memory}\n${newMemory}` :
+            newMemory;
+        await docRef.update({ memory: updatedMemory });
+    } else {
+        await docRef.set({ memory: newMemory });
+    }
+}
