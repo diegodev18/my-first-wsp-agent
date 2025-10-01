2cde6d089f8e894b9d17c83f48e58a63d4bcf725
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-01 17:29:25 -0600

    Refactor get function to improve document reference handling in memory collection

--- functions/src/utils/db/memory.js
+++ functions/src/utils/db/memory.js
@@ -1,13 +1,19 @@
 import { db } from "../../lib/firebase.js";
 
 export const get = async (waId) => {
-    const docRef = db.collection("memory").doc(waId);
+    const collection = db.collection("memory");
+
+    const docRef = collection.doc(waId);
+    if (!docRef) {
+        collection.add({ waId, memory: "" });
+        return "";
+    }
 
     const doc = await docRef.get();
-
     if (doc.exists) {
         return doc.data().memory || "";
     } else {
+        docRef.set({ memory: "" });
         return "";
     }
 };
