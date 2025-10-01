9f38ebc91e88fc53c152190baf212880bdd3ecb5
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-01 00:24:22 -0600

    Refactor app.js to import PORT from config and remove local declaration

--- functions/src/app.js
+++ functions/src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 
+import { PORT } from "./config";
+
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
