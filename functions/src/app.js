be0ee2110eabeff690dcd4f8525c82ae3f603ae7
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-01 00:44:56 -0600

    Remove PORT import and server listen logic from app.js

--- functions/src/app.js
+++ functions/src/app.js
@@ -1,15 +1,11 @@
 import express from "express";
 
-import { PORT } from "./config.js";
-
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
 
 export default app;
