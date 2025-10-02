aa2ce7bfd01847a198d2d4139a34ca60d69673fb
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-01 18:56:15 -0600

    Update promptNewMemory to include detailed user memory format instructions

--- functions/src/utils/llm/prompt.js
+++ functions/src/utils/llm/prompt.js
@@ -10,6 +10,6 @@ export const promptNewMemory = (userMessage, userMemory) => {
     return `\
 Esto es lo que ya sabes sobre el usuario: ${userMemory}.
 Este es el mensaje del usuario: "${userMessage}".
-En el siguiente mensaje, ¿Hay algo que debas recordar para futuras conversaciones y no este en tu memoria?. Recuerda este detalle importante. Si no hay nada que recordar, responde unicamente un "${noMemoryResponse}", y si hay algo que recordar, por favor compártelo. Es muy importante que sigas estas instrucciones.\
+En el siguiente mensaje, ¿Hay algo que debas recordar para futuras conversaciones y no este en tu memoria?. Recuerda este detalle importante. Si no hay nada que recordar, responde unicamente un "${noMemoryResponse}", y si hay algo que recordar, por favor compártelo. Es muy importante que sigas estas instrucciones. Usa el siguiente formato: "El usuario es de Mexico, tiene 18 años, nacio en Tabasco. El usuario desarrolló una nueva habilidad en programación. El usuario le gusta programar en Python, JavaScript y C++."\
 `;
 }
