48072f89515dbcf0f9bf3a89ed84b9f25a90079f
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-01 23:59:46 -0600

    Add JSDoc comments for fetchData function parameters and return type

--- functions/src/utils/github/api.js
+++ functions/src/utils/github/api.js
@@ -1,4 +1,9 @@
 
+/**
+ * @param {string} url 
+ * @param {null | string} authToken 
+ * @returns {Promise<null | Object>}
+ */
 export const fetchData = async (url, authToken = null) => {
     const headers = authToken ? { Authorization: `token ${authToken}` } : {};
     const response = await fetch(url, { headers });
