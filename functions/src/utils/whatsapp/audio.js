5ae2c3554cc697e2cd601db14eec1459eb851cf9
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-03 11:51:38 -0600

    Refactor downloadAudio to get and return audio as base64 instead of saving to file

--- functions/src/utils/whatsapp/audio.js
+++ functions/src/utils/whatsapp/audio.js
@@ -4,7 +4,7 @@ import { WHATSAPP_ACCESS_TOKEN } from "../../config.js";
 
 fs.mkdirSync("/tmp", { recursive: true });
 
-export const downloadAudio = async (mediaId) => {
+export const get = async (mediaId) => {
     const response = await fetch(`https://graph.facebook.com/v22.0/${mediaId}`, {
         method: "GET",
         headers: {
@@ -16,19 +16,21 @@ export const downloadAudio = async (mediaId) => {
     const data = await response.json();
     if (!data || !data.url) return null;
 
-    const timestamp = Date.now().toString();
-    const filePath = `/tmp/audio_${timestamp}.ogg`;
-
     try {
         const audioDownloaded = await fetch(data.url, {
             headers: {
                 "Authorization": `Bearer ${WHATSAPP_ACCESS_TOKEN}`
             },
-            output: filePath
         });
         if (!audioDownloaded.ok) return null;
 
-        return filePath;
+        const arrayBuffer = await audioDownloaded.arrayBuffer();
+
+        const buffer = Buffer.from(arrayBuffer);
+
+        const base64Data = buffer.toString("base64");
+
+        return base64Data;
     } catch (err) {
         console.error("Error downloading audio file:", err);
         return null;
